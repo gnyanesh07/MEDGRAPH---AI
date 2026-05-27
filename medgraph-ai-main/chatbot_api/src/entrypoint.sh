98347d00bad1d088be87bfd8b84069cd0fef0363
gnyanesh07 <gnyanesh0451@gmail.com>
2026-05-28 03:20:18 +0530

    Delete medgraph-ai-main directory

--- medgraph-ai-main/chatbot_api/src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting hospital RAG FastAPI service..."
-
-uvicorn main:app --host 0.0.0.0 --port 8000
--- medgraph-ai-main/chatbot_frontend/src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting hospital chatbot frontend..."
-
-streamlit run main.py
